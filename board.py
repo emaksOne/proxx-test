235a0e28ed2c672138575e383d1a635deaed7c97
Maksym <makssw94@gmail.com>
2022-08-20 01:31:19 +0300

    add annotations

--- board.py
+++ board.py
@@ -1,39 +1,75 @@
 import random
 from collections import deque
+from typing import Union
 
 from cell import Cell
 
 
 class Board:
-    def __init__(self, size):
+    def __init__(self, size: int):
+        """Init Board instance
+
+        Args:
+            size (int): size of the board
+        """
         self.size = size
+        # create (size x size) matrix of cells
         self.board = [[Cell() for _ in range(size)] for _ in range(size)]
-    
-    def _update_adjacent(self, bh_i, bh_j):
-        for i in range(max(0, bh_i-1), min(bh_i+1, self.size-1) + 1):
-            for j in range(max(0, bh_j-1), min(bh_j+1, self.size-1) + 1):
+
+    def _update_adjacent(self, bh_i: int, bh_j: int):
+        """Helper method to update adjacent cells with +1
+
+        Args:
+            bh_i (int): black hole row position
+            bh_j (int): black hole col position
+        """
+        for i in range(max(0, bh_i - 1), min(bh_i + 1, self.size - 1) + 1):
+            for j in range(max(0, bh_j - 1), min(bh_j + 1, self.size - 1) + 1):
                 if i == bh_i and j == bh_j:
                     continue
                 self.board[i][j].increase_number_of_adjacent_bh()
 
-    def populate(self, k, seed=None):
-        if k > self.size ** 2:
-            error = "K cannot be bigger then number of cells in the board. " + \
-                f"Choose K less or equal than {self.size ** 2}"
+    def populate(self, k: int, seed: Union[int, None] = None):
+        """Populate board with K black holes
+
+        Args:
+            k (int): number of black holes
+            seed (Union[int, None], optional): seed for random. Defaults to None.
+
+        Raises:
+            Exception: K is not valid
+        """
+
+        if k > self.size**2 or k < 0:
+            error = "K is not valid"
             raise Exception(error)
 
         if seed is not None:
             random.seed(seed)
 
+        # imagine if we unroll matrix to single vector
+        # and pick random k indexes in this vector
         indexes = random.sample(list(range(self.size**2)), k)
 
         for index in indexes:
+            # convert vector index to matrix i, j indexes
             i = index // self.size
             j = index % self.size
             self.board[i][j].set_bh()
+            # update adjacent cells (+1)
             self._update_adjacent(i, j)
 
-    def click_cell(self, i, j):
+    def click_cell(self, i: int, j: int):
+        """Update board when click on cell
+
+        Args:
+            i (int): cell's row position
+            j (int): cell's col position
+
+        Raises:
+            Exception: not valid cell's click position
+            Exception: click on the black hole
+        """
         if not 0 <= i < self.size or not 0 <= j < self.size:
             error = "Position out of bounds"
             raise Exception(error)
@@ -45,16 +81,18 @@ class Board:
         # check if it is already opened
         if self.board[i][j].is_open():
             return
-        
+
         if self.board[i][j].get_number_of_adjacent_bh() > 0:
             self.board[i][j].set_open()
         else:
             # need to open all surrounding zero cells
             # apply BFS
+
+            # for storing processed cells
             seen = set()
             q = deque()
 
-            q.append((i,j))
+            q.append((i, j))
             while q:
                 row, col = q.popleft()
                 self.board[row][col].set_open()
@@ -63,19 +101,25 @@ class Board:
                 if self.board[row][col].get_number_of_adjacent_bh() > 0:
                     continue
 
-                for r in range(max(0, row-1), min(row+1, self.size-1) + 1):
-                    for c in range(max(0, col-1), min(col+1, self.size-1) + 1):
-                        if (r, c) in seen:
+                # add neighbor cells to queue
+                for r in range(max(0, row - 1), min(row + 1, self.size - 1) + 1):
+                    for c in range(max(0, col - 1), min(col + 1, self.size - 1) + 1):
+                        # skip processed or already open cell
+                        if (r, c) in seen or self.board[r][c].is_open():
                             continue
-                        q.append((r,c))
+                        q.append((r, c))
 
+    def _show(self, all_opened: bool = False):
+        """Display board. For debug purpose
 
-    def _show(self, ignore_closed=False):
-        print("--"*(self.size*2))
+        Args:
+            all_opened (bool, optional): If it is True then consider that all cells is open. Defaults to False.
+        """
+        print("--" * (self.size * 2))
         for line in self.board:
             values = []
             for cell in line:
-                if not ignore_closed and not cell.is_open():
+                if not all_opened and not cell.is_open():
                     values.append("X")
                     continue
                 if cell.is_bh():
@@ -84,6 +128,4 @@ class Board:
                 values.append(str(cell.get_number_of_adjacent_bh()))
             line_str = "| " + " | ".join(values) + " |"
             print(line_str)
-            print("--"*(self.size*2))
-    
-    
+            print("--" * (self.size * 2))
--- cell.py
+++ cell.py
@@ -1,23 +1,46 @@
 class Cell:
     def __init__(self):
+        """Init cell instance
+        By default it is not open,
+        not black hole
+        and has 0 adjacent black holes
+        """
         self._is_open = False
         self._is_bh = False
         self._number_of_adjacent_bh = 0
 
     def set_open(self):
+        """Set cell as open"""
         self._is_open = True
-    
+
     def is_open(self):
+        """Check if cell is open
+
+        Returns:
+            bool: is open
+        """
         return self._is_open
 
     def set_bh(self):
+        """Set cell as black hole"""
         self._is_bh = True
 
     def is_bh(self):
+        """Check if cell is black hole
+
+        Returns:
+            bool: is black hole
+        """
         return self._is_bh
 
     def get_number_of_adjacent_bh(self):
+        """Get number of adjacent black holes
+
+        Returns:
+            int: number of adjacent black holes
+        """
         return self._number_of_adjacent_bh
-    
+
     def increase_number_of_adjacent_bh(self):
+        """Increase number of adjacent black holes by one"""
         self._number_of_adjacent_bh += 1
