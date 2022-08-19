97815d41a0bb1b33e4be6023a4d2707c6e78c305
Maksym <makssw94@gmail.com>
2022-08-20 00:34:51 +0300

    part 4: click cell

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import random
+from collections import deque
 
 
 class Cell:
@@ -55,12 +56,52 @@ class Board:
             self.board[i][j].set_bh()
             self._update_adjacent(i, j)
 
+    def click_cell(self, i, j):
+        if not 0 <= i < self.size or not 0 <= j < self.size:
+            error = "Position out of bounds"
+            raise Exception(error)
 
-    def _show(self):
+        # check if it is black hole
+        if self.board[i][j].is_bh():
+            raise Exception("You lose")
+
+        # check if it is already opened
+        if self.board[i][j].is_open():
+            return
+        
+        if self.board[i][j].get_number_of_adjacent_bh() > 0:
+            self.board[i][j].set_open()
+        else:
+            # need to open all surrounding zero cells
+            # apply BFS
+            seen = set()
+            q = deque()
+
+            q.append((i,j))
+            while q:
+                row, col = q.popleft()
+                self.board[row][col].set_open()
+                seen.add((row, col))
+                # if we encounter not zero cell - stop expanding from this cell
+                if self.board[row][col].get_number_of_adjacent_bh() > 0:
+                    continue
+
+                for r in range(max(0, row-1), min(row+1, self.size-1) + 1):
+                    for c in range(max(0, col-1), min(col+1, self.size-1) + 1):
+                        if (r, c) in seen:
+                            continue
+                        q.append((r,c))
+
+
+
+    def _show(self, ignore_closed=False):
         print("--"*(self.size*2))
         for line in self.board:
             values = []
             for cell in line:
+                if not ignore_closed and not cell.is_open():
+                    values.append("X")
+                    continue
                 if cell.is_bh():
                     values.append("H")
                     continue
@@ -68,5 +109,5 @@ class Board:
             line_str = "| " + " | ".join(values) + " |"
             print(line_str)
             print("--"*(self.size*2))
- 
+    
     
